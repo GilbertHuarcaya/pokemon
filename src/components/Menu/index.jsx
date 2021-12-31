5e4904e21ea3788fbee49f89a8339a70cbb1d8f6
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2021-12-30 21:01:44 -0500

    Created menu

---
+++ src/components/Menu/index.jsx
@@ -0,0 +1,59 @@
+import { useLocation, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import "./styles.scss";
+
+const Menu = () => {
+  const [toggleClassBtnMenu, setToggleCLassBtnMenu] = useState("false");
+  const [pokemonLink, setPokemonLnk] = useState();
+
+  const handlerMenu = () => {
+    if (!toggleClassBtnMenu) return setToggleCLassBtnMenu(true);
+    return setToggleCLassBtnMenu(false);
+  };
+  const urlactual = useLocation().pathname;
+  const pattern = /\/[0-9]+$/;
+
+  useEffect(() => {
+    setPokemonLnk(urlactual.match(pattern));
+  }, [useLocation()]);
+
+  return (
+    <div className="Menu">
+      <nav
+        className={
+          toggleClassBtnMenu
+            ? "Menu__navbar"
+            : "Menu__navbar is-active-menu-navbar"
+        }
+      >
+        <Link to="/" className="Menu__navbar__a" onClick={handlerMenu}>
+          Home
+        </Link>
+        {!pattern.test(urlactual) ? null : (
+          <Link
+            to={`/detalle${pokemonLink}`}
+            className="Menu__navbar__a"
+            hidden={!pattern.test(urlactual)}
+            onClick={handlerMenu}
+          >
+            Pokemon
+          </Link>
+        )}
+
+        <Link to="/About" className="Menu__navbar__a" onClick={handlerMenu}>
+          About
+        </Link>
+      </nav>
+      <div className="Menu__logo">
+        <button
+          className="Menu__logo__button"
+          type="button"
+          aria-label="Menu"
+          onClick={handlerMenu}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
