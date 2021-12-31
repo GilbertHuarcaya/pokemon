279fb6c0f8ef58f73a67f00195735921f12a6304
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2021-12-30 21:01:33 -0500

    Created Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import "./styles.scss";
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <h1 className="Hedader_title">The Pokedex</h1>
+    </div>
+  );
+};
+
+export default Header;
